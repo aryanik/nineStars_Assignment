31c05a8670621b8582d5bf1814a4e33e07cb366a
ravimishra0705 <ravi@deotechsolutions.com>
2025-02-11 21:18:05 +0530

    change api key

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,2 +1,2 @@
-export const API_KEY = '018323997ec14eb8873652039a0a4152';
+export const API_KEY = '8707722bd7994ae391c3f5bac1ae16ff';
 export const BASE_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
